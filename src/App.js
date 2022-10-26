29db0f6dd304b638097e68a6e93776bc111d8fe9
managerlist89 <managerlist89@gmail.com>
2022-10-26 23:15:20 +0900

    update-wise

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With us
+          Mr.Wise is an Idiot!!!
         </a>
       </header>
     </div>
